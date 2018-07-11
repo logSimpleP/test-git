f59de5658f36903b226354ecd87a92407e618821
pg <691846981@qq.com>
2018-07-11 15:47:51 +0800

    第四次提交

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,4 +2,13 @@
  * Created by 69184 on 2018/7/11.
  */
 public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
