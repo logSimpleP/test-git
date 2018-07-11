7f24e013e2994232cf40c57c1a5c4c6e7311efcb
pg <691846981@qq.com>
2018-07-11 16:22:23 +0800

    new commit

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,4 +2,22 @@
  * Created by 69184 on 2018/7/11.
  */
 public class User {
+    private String sname;
+    private String password;
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
